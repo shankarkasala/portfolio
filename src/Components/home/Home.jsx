052361f6a7e60adcbe96cbb2e60f74a1db61d59c
shankarkasala <umashankarno66@gmail.com>
2022-05-17 20:24:30 +0530

    implimented skill component and typing effect in home page

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 import "./home.css";
 function Home() {
   return (
     <div className="bg-img">
       <div className="text">
         <h1>Hello,</h1>
+
         <h1> i'm kasala umasankar.</h1>
         <h2>A Front End Developer.</h2>
-        <p> I build things for the web.</p>
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+
+              .typeString("Welcome ,")
+
+              .pauseFor(1000)
+              .deleteAll()
+              .typeString(" I build things for the web.")
+              .start();
+          }}
+        />
+        {/* <p> I build things for the web.</p> */}
       </div>
     </div>
   );
--- src/Components/services/Services.jsx
+++ src/Components/services/Services.jsx
@@ -5,21 +5,68 @@ import html from "./html.png";
 import css from "./css.png";
 import reactlogo from "./logo-react-icon.png";
 import typelogo from "./typescript.png";
-// import bootstrap from "./bootstrap-log.png";
 import material from "./Material-UI.png";
 function Services() {
   return (
     <div className="skills-box">
       <div className="skills-container">
         <div className="skills-left">
-          <img src={jslogo} alt="javascript" />
-          <img src={html} alt="html" />
-          <img src={css} alt="css" />
+          <div className="skills-javascript">
+            <img src={jslogo} alt="javascript" />
+            <div className="skills-bar-wrap">
+              <h6 className="skills-h6">JavaScript</h6>
+              <div className="skills-bar-container">
+                <div className="skills-bar-javascript"></div>
+              </div>
+            </div>
+          </div>
+          <div className="skills-html">
+            <img src={html} alt="html" />
+            <div className="skills-bar-wrap">
+              <h6 className="skills-h6">HTML5</h6>
+              <div className="skills-bar-container">
+                <div className="skills-bar-html"></div>
+              </div>
+            </div>
+          </div>
+          <div className="skills-css">
+            <img src={css} alt="css" />
+            <div className="skills-bar-wrap">
+              <h6 className="skills-h6">CSS3</h6>
+              <div className="skills-bar-container">
+                <div className="skills-bar-css"></div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="skills-right">
-          <img src={typelogo} alt="typescript" />
-          <img src={reactlogo} alt="react" />
-          <img src={material} alt="materialui" />
+          <div className="skills-typescript">
+            <img src={typelogo} alt="typescript" />
+            <div className="skills-bar-wrap-1">
+              <h6 className="skills-h6">Typescript</h6>
+              <div className="skills-bar-container-1">
+                <div className="skills-bar-typescript"></div>
+              </div>
+            </div>
+          </div>
+          <div className="skills-react">
+            <img src={reactlogo} alt="react" />
+            <div className="skills-bar-wrap-1">
+              <h6 className="skills-h6">React JS</h6>
+              <div className="skills-bar-container-1">
+                <div className="skills-bar-react"></div>
+              </div>
+            </div>
+          </div>
+          <div className="skills-materialui">
+            <img src={material} alt="materialui" />
+            <div className="skills-bar-wrap-1">
+              <h6 className="skills-h6">Material UI</h6>
+              <div className="skills-bar-container-1">
+                <div className="skills-bar-materialui"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
